e0f2f0eae5ed72cd49c329e84f538c3071fbd5f9
Robert Lord <robert@codepeak.se>
2016-10-28 14:19:36 +0200

    Changes for packagist.org

--- registration.php
+++ registration.php
@@ -1,6 +1,6 @@
 <?php
 \Magento\Framework\Component\ComponentRegistrar::register(
     \Magento\Framework\Component\ComponentRegistrar::LANGUAGE,
-    'codepeak_sv_SE',
+    'codepeak_sv_se',
     __DIR__
 );
